f40ada25efe718b55665e25d7a095d54e2e164a7
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2024-02-05 18:34:11 +0100

    creato pagina visualizzazione singolo fumetto utilizzando url dedicato in base all'id

--- resources/views/guest/comics/show.blade.php
+++ resources/views/guest/comics/show.blade.php
@@ -1,2 +1,22 @@
 
-<p>sezione show </p>
+@extends('layout.app')
+
+@section('main-section')
+<div class="container">
+    <div class="row">
+            <div class="col-12">
+                <div class="card mx-2" style="width: 18rem;">
+                    <img src="{{ $comics['thumb'] }}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <ul>
+                        <li class="list-group-item mb-1">{{ $comics['title'] }}</li>
+                        <li class="list-group-item mb-1">{{ $comics['description'] }}</li>
+                        <li class="list-group-item mb-1">{{ $comics['series'] }}</li>
+                        <li class="list-group-item mb-1">{{ $comics['price'] }}</li>
+                      </ul>
+                    </div>
+                  </div>
+            </div>
+    </div>
+</div>
+@endsection
