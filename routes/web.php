bab3528fe966d147ec9683af6e092ac9daaa583e
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2024-02-06 15:35:37 +0100

    creato controller per create

--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,5 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', [ComicController::class, 'index'])->name('guest.index');
-
+Route::get('/create', [ComicController::class, 'create'])->name('guest.create');
 Route::get('/{id}', [ComicController::class, 'show'])->name('guest.show');
