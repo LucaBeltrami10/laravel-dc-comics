8dea95dbceffcde3574ef836e3b64d842c91c4f9
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2024-02-06 15:34:18 +0100

    aggiunto in a header href con route a index e create

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,6 +1,6 @@
 <div class="mx-auto">
     <ul class="d-flex justify-content-center align-items-center mt-3">
-        <li class="me-5"><a href="">Comics</a></li>
-        <li class="me-5"><a href="">Aggiungi Comic</a></li>
+        <li class="me-5"><a href="{{ route('guest.index') }}">Comics</a></li>
+        <li class="me-5"><a href="{{ route('guest.create') }}">Aggiungi Comic</a></li>
     </ul>
 </div>
