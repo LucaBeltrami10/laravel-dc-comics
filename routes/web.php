c8648800cee8dbf45be1b6d3c155dfdaf4430f75
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2024-02-06 16:18:12 +0100

    creato rotta store

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [ComicController::class, 'index'])->name('guest.index');
 Route::get('/create', [ComicController::class, 'create'])->name('guest.create');
+Route::post('/', [ComicController::class, 'store'])->name('guest.store');
 Route::get('/{id}', [ComicController::class, 'show'])->name('guest.show');
