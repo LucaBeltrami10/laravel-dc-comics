ec726c9041ece40f1c2bf14c7bd762ea0dc58be2
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2024-02-06 15:20:49 +0100

    aggiunto due li a ul in header e modificato style

--- resources/views/guest/comics/index.blade.php
+++ resources/views/guest/comics/index.blade.php
@@ -6,12 +6,11 @@
         @foreach ($comics as $comic)
             <div class="col-3">
                 <a href="{{ route('rottaDiProva', $comic['id']) }}">
-                    <div class="card mx-2" style="width: 18rem;">
+                    <div class="card mx-2">
                         <img src="{{ $comic['thumb'] }}" class="card-img-top" alt="...">
                         <div class="card-body">
                         <ul>
                             <li class="list-group-item mb-1">{{ $comic['title'] }}</li>
-                            <li class="list-group-item mb-1">{{ $comic['description'] }}</li>
                             <li class="list-group-item mb-1">{{ $comic['series'] }}</li>
                             <li class="list-group-item mb-1">{{ $comic['price'] }}</li>
                         </ul>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,2 +1,6 @@
-
-<p>header</p>
+<div class="mx-auto">
+    <ul class="d-flex justify-content-center align-items-center mt-3">
+        <li class="me-5"><a href="">Comics</a></li>
+        <li class="me-5"><a href="">Aggiungi Comic</a></li>
+    </ul>
+</div>
