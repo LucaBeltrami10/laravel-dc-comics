00558351da6b37db6265606ebfe29c60daf5cc74
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2024-02-07 15:44:17 +0100

    aggiunta rotta update

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,4 @@ Route::get('/create', [ComicController::class, 'create'])->name('guest.create');
 Route::post('/', [ComicController::class, 'store'])->name('guest.store');
 Route::get('/{id}/edit', [ComicController::class, 'edit'])->name('guest.edit');
 Route::get('/{id}', [ComicController::class, 'show'])->name('guest.show');
+Route::put('/{id}', [ComicController::class, 'update'])->name('guest.update');
