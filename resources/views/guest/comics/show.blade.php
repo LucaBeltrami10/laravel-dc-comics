cc8f34f1031ce1c8207edcc58751f9aba42e93ca
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2024-02-07 14:55:49 +0100

    modificato style di show e aggiunto button per modificare

--- resources/views/guest/comics/show.blade.php
+++ resources/views/guest/comics/show.blade.php
@@ -4,16 +4,44 @@
 @section('main-section')
 <div class="container">
     <div class="row">
-            <div class="col-12">
-                <div class="card mx-2 text-center" style="width: 18rem;">
+            <div class="col-6">
+                <div class="card mx-2 text-center mx-auto" style="width: 18rem;">
                     <img src="{{ $comics['thumb'] }}" class="card-img-top" alt="...">
                     <div class="card-body">
-                    <ul>
-                        <li class="list-group-item mb-1">{{ $comics['title'] }}</li>
-                        <li class="list-group-item mb-1">{{ $comics['description'] }}</li>
-                        <li class="list-group-item mb-1">{{ $comics['series'] }}</li>
-                        <li class="list-group-item mb-1">{{ $comics['price'] }}</li>
-                    </ul>
+                        <a href="" class="btn btn-primary">Modifica</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-6">
+                <div class="card mx-2 text-center mx-auto" style="width: 18rem;">
+                    <div class="card-body">
+                        <ul class="p-0">
+                            <li class="list-group-item">
+                                <div>
+                                    <p class="m-0 fw-bold">Titolo:</p>
+                                    <p class="m-0">{{ $comics['title'] }}</p>
+                                </div>
+                            </li>
+                            <li class="list-group-item">
+                                <div>
+                                    <p class="m-0 fw-bold">Series:</p>
+                                    <p class="m-0">{{ $comics['series']  }}</p>
+                                </div>
+                            </li>
+                            <li class="list-group-item">
+                                <div>
+                                    <p class="m-0 fw-bold">Series:</p>
+                                    <p class="m-0">{{ $comics['description']  }}</p>
+                                </div>
+                            </li>
+                            <li class="list-group-item">
+                                <div>
+                                    <p class="m-0 fw-bold">Prezzo:</p>
+                                    <p class="m-0">{{ $comics['price'] }}</p>
+                                </div>
+                            </li>
+                        </ul>
+                        <a class="btn btn-primary" href="">Modifica</a>
                     </div>
                 </div>
             </div>
